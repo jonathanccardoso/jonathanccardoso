9ecce24607b3e1b376dabb686b204c2a27de6fe8
jonathanccardoso <jonathan99moura@hotmail.com>
2020-03-28 13:06:44 -0300

    Fix: adds styles and links on component

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,16 +2,16 @@ module.exports = {
   siteMetadata: {
     title: `Jonathan Cardoso`,
     description: `Full Stack development. Living and learning!.`,
-    author: `@jonathanccardoso`,
+    author: `@jonathanccardoso`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        name: `assets`,
+        path: `${__dirname}/content/assets`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -24,12 +24,11 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+        icon: `content/assets/gatsby-icon.png`, // This path is relative to the root of the site.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-  // pathPrefix: "/jonathanccardoso.github.io",
-}
+  ]
+};
--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,74 +2,36 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 
-const Header = ({ siteTitle }) => (
-  // <header
-  //   style={{
-  //     background: `rebeccapurple`,
-  //     marginBottom: `1.45rem`,
-  //   }}
-  // >
-  //   <div
-  //     style={{
-  //       margin: `0 auto`,
-  //       maxWidth: 960,
-  //       padding: `1.45rem 1.0875rem`,
-  //     }}
-  //   >
-  //     <h1 style={{ margin: 0 }}>
-  //       <Link
-  //         to="/"
-  //         style={{
-  //           color: `white`,
-  //           textDecoration: `none`,
-  //         }}
-  //       >
-  //         {siteTitle}
-  //       </Link>
-  //     </h1>
-  //   </div>
-  // </header>
+import "./header.css";
 
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`
-      }}
-    >
+const Header = ({ siteTitle }) => (
+  <header>
+    <div>
       <nav>
-        <input type="checkbox" id="nav" class="hidden" />
-        <label for="nav" class="nav-btn">
+        <input type="checkbox" id="nav" />
+        <label for="nav" className="nav-btn">
           <i></i>
           <i></i>
           <i></i>
         </label>
-        <div class="logo">
-          <h1 style={{ margin: 0 }}>
-            <Link
-              to="/"
-              style={{
-                color: `white`,
-                textDecoration: `none`
-              }}
-            >
-              {siteTitle}
-            </Link>
+        <div className="logo">
+          <h1>
+            <Link to="/">{siteTitle}</Link>
           </h1>
         </div>
-        <div class="nav-wrapper">
+        <div className="nav-wrapper">
           <ul>
             <li>
-              <a href="/about">About</a>
+              <Link to="/about/">about</Link>
             </li>
             <li>
-              <a href="/services">Services</a>
+              <Link to="/portfolio/">portfolio</Link>
+            </li>
+            {/* <li>
+              <a href="#services">blog</a>
+            </li> */}
+            <li>
+              <Link to="/links/">links</Link>
             </li>
           </ul>
         </div>
